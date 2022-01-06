af816a68cfc72377a59796178d2a29e90352727a
Eshan Chatterjee <eshan2001221@gmail.com>
2022-01-06 17:43:15 +0530

    init commit

---
+++ src/print.rs
@@ -0,0 +1,21 @@
+pub fn run()
+{
+    // STDOUT print
+    println!("Hello From the print.rs File");
+
+    // Formatting Basic
+    println!("Number: {}", 1);
+    println!("{} is from {}", "Brad", "Mass");
+
+    // Positional Parameters
+    println!("{0} is from {1} and {0} likes to {2}", "Brad", "Mass", "Code");
+
+    // Named Arguments
+    println!("{name} likes to play {activity}", name = "John", activity = "Baseball");
+
+    // Placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+
+    // Placeholder for debug trait
+    println!("{:?}", (12, true, "hello"));
+}
